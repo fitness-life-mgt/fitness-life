3eca33f81b8670696c3823c1f744ae03c8e8c7fa
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-07-01 10:57:04 +0530

    card1

---
+++ frontend/src/components/card1.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container,Row,Col } from 'react-bootstrap';
+
+const card1 = () => {
+        return (
+        <Container className ="background">
+            <Row>
+                <Col>
+                < img src="./image/im1.jpg"></img>
+                </Col>
+                <Col>
+                <p>jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</p>
+                </Col>
+
+            </Row>
+        </Container>
+    )
+}
+
+export default card1;
