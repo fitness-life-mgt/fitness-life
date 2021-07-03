1e1f0e01c9dd530cf844d227d1c4ea4da1bfebdf
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-07-03 23:18:19 +0530

    update home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import  Header from './components/Header';
-import Card1 from './components/card1';
+
 
 function App() {
   return (
    <>
     <Navbar/>  
     <Header/> 
-    <Card1/>   
+     
     </>
     );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Typed from 'react-typed';
+import im2 from "./image/im2.jpg";
+import im3 from "./image/im3.jpg";
+import im4 from "./image/im4.jpg";
+import { Container,Row,Col,Figure } from 'react-bootstrap';
+
 
 const Header = () => {
     return (
+        <>
        <div className="header-wrapper">
            <div className="main-info">
                <h1>Let's Do This, Together​!</h1>
@@ -15,7 +21,104 @@ const Header = () => {
                />
            </div>
        </div>
+       <div className="line"></div>       
+       <div className="card1-wrapper">
+       <div class="row">
+                    <div class="col-md-auto">
+                            <div class="b1">
+                                    <img src ={im2} alt="Paris" className="imclz" />  
+                            </div>
+                    </div>
+                    <div class="col col-lg-2">
+                    <div class="text-block">
+                        <h2>Get Started with FitnessLife!</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <button class="button1 " type="button">Join Us Now!</button>
 
+                    </div>
+                    </div>                   
+           </div>                                      
+        </div>
+        <div className="line"></div>    
+        <div className="card2-wrapper">
+                <div class="row">
+                    <div class="col col-lg-2">
+                        <div class="text-block2">
+                            <h2>Workout at home with ease!</h2>
+                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                             </p>
+                             <button class="button1 " type="button">Join Us Now!</button>
+
+                        </div>
+                    </div>  
+                    <div class="col-md-auto">
+                            <div class="b2">
+                                    <img src ={im3} alt="Paris" className="imclz2" />  
+                            </div>
+                    </div>                 
+                </div>                                      
+        </div> 
+        <div className="line"></div> 
+        <div className="card1-wrapper">
+            <div class="container">
+                <div class="row justify-content-md-center">
+                        <div class="col col-lg-4">
+                            <div class="sh1">
+                                <h2>Shop</h2>
+                                <p></p>
+                            </div>    
+                        </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col col-lg-4">
+                        <div class="responsive">
+                            <div class="gallery">
+                                <a target="_blank" href={im2}>
+                                <img src={im2} alt="Cinque Terre" width="300px" height="400"/>
+                                </a>
+                                <div class="desc">
+                            <button class="button2 " type="button">Sports Wears</button>
+                            </div>
+                            </div>
+                            </div>
+                    </div>
+                    <div class="col col-lg-4">  
+                    <div class="responsive">
+                        <div class="gallery">
+                            <a target="_blank" href={im2}>
+                            <img src={im2} alt="Cinque Terre" width="300px" height="400"/>
+                            </a>
+                            <div class="desc">
+                            <button class="button2 " type="button">Suppliments</button>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col col-lg-2">
+                        <div class="responsive">
+                            <div class="gallery">
+                                <a target="_blank" href={im2}>
+                                <img src={im4} alt="Cinque Terre" width="300px" height="10"/>
+                                </a>
+                                <div class="desc">
+                                    <button class="button2 " type="button">Accessories</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>                         
+        </div>
+        <div className="line"></div> 
+
+
+    </>
     )
 }
 
