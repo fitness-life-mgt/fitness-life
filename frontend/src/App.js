5dd0cea2a63d655dbc88beb7d92f89cfe33ed08b
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-07-05 22:34:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import  Header from './components/Header';
-
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Workouts from './Workouts';
+import Login from './Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-   <>
+    <Router>  
     <Navbar/>  
-    <Header/> 
-     
-    </>
+    <Switch>
+    <Route path='/' exact component={Home} />
+    <Route path='/About' exact component={About} />
+    <Route path='/Workouts' exact component={Workouts} />
+    <Route path='/Login' exact component={Login} />
+    
+    </Switch>    
+    </Router>  
     );
 }
 
