14dbae3396dfb947a3979dd184c517c9a1427bea
Maleesha Dinoli Silva <56461176+maleeshasilva@users.noreply.github.com>
2021-09-20 09:46:56 +0530

    added advanced workouts

--- backup/server/routes/workouts.js
+++ backup/server/routes/workouts.js
@@ -35,8 +35,70 @@ Router.post("/beginner", (req, res) => {
 });
 
 
+Router.post("/intermediate", (req, res) => {
+  
+    db.query(
+        "SELECT intermediate FROM member WHERE firstName ='Rusiru'",
+            (err,result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                
+                var enrollmentt =result[0].intermediate;
+                if(enrollmentt==1){
+                    //already enrolled
+                   // console.log("You have already Enrolled!.");
+                    return res.send("You have already Enrolled!.");
+                }else if(enrollmentt==0){
+                    db.query(
+                        "UPDATE member SET intermediate = 1 WHERE firstName ='Rusiru'",
+                        (err2,result2)=>{
+                            if(err2){
+                                console.log(err); 
+                            }else{
+                                //console.log("Successfully Enrolled!.");
+                                return res.send("Successfully Enrolled!.");                               
+                            }
+                        });
+                }
+                
+            }
+        }
+    );
+});
 
 
+Router.post("/advanced", (req, res) => {
+  
+    db.query(
+        "SELECT advanced FROM member WHERE firstName ='Rusiru'",
+            (err,result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                
+                var enrollmentt =result[0].advanced;
+                if(enrollmentt==1){
+                    //already enrolled
+                   // console.log("You have already Enrolled!.");
+                    return res.send("You have already Enrolled!.");
+                }else if(enrollmentt==0){
+                    db.query(
+                        "UPDATE member SET advanced = 1 WHERE firstName ='Rusiru'",
+                        (err2,result2)=>{
+                            if(err2){
+                                console.log(err); 
+                            }else{
+                                //console.log("Successfully Enrolled!.");
+                                return res.send("Successfully Enrolled!.");                               
+                            }
+                        });
+                }
+                
+            }
+        }
+    );
+});
 
 
 module.exports = Router;
